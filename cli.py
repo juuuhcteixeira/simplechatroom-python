8aa6acd4694251db8c9c7b5b98cca37244e49ced
Julia Teixeira <juuhc.teixeira@gmail.com>
2020-08-30 21:11:14 -0300

    nova versao

--- cli.py
+++ cli.py
@@ -3,52 +3,63 @@ import select
 import socket
 import sys
 
-# Informacoes do servidor ao qual se conectar
+# Informacoes do servidor ao qual se conectar e deve ser o mesmmo do server.py
 HEARDER_SIZE = 10
 IP = "127.0.0.1"
 PORT = 4200
 
-my_user = input("User: ")
+# Recebe o nome do usuario
+my_user = input("Usuario: ")
 
 # Criando socket para comunicacao
 socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_tcp.connect((IP, PORT))
 socket_tcp.setblocking(False)
 
+# Criando os status do usario
 username = my_user.encode('utf-8')
 username_header = f'{len(username):<{HEARDER_SIZE}}'.encode('utf-8')
 socket_tcp.send(username_header + username)
 
-while True:
-    message = input(f'{my_user} > ')
-    # message = ""
+# criar loop para receber e mandar mensagens
 
+while True:
+    #Enquanto a pessoa digita aparece o nome dela e que ela está digitando
+    message = input(f'{my_user} > ')
+
+    #Enviando a mensagem
     if message:
         message = message.encode('utf-8')
         message_header = f'{len(message) :< {HEARDER_SIZE}}'.encode('utf-8')
         socket_tcp.send(message_header + message)
     try:
         while True:
-            # receive msg
+            # Para receber a mensagens
             username_header = socket_tcp.recv(HEARDER_SIZE)
-            if not len(username_header):
-                print('connection closed by the server')
-                sys.exit()
+            if not len(username_header): #Nao receber nenhum dado
+                print('conexao fechada')
+                sys.exit() #fecha a conexao com o servidor
 
+            # Para receber os dados do cliente
             username_length = int(username_header.decode('utf-8').strip())
             username = socket_tcp.recv(username_length).decode('utf-8')
 
+            # Para receber a mensagem
             message_header = socket_tcp.recv(HEARDER_SIZE)
             message_length = int(message_header.decode('utf-8').strip())
             message = socket_tcp.recv(message_length).decode('utf-8')
+
+            # Depois de receber os dados e mensagens, imprimimos 
             print(f'{username} > {message}')
+
+    # Tratar erros nas mensagens
 
     except IOError as e:
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('REading error', str(e))
+            print('Lendo erro', str(e))
 
         continue
 
     except Exception as e:
-        print('General error', str(e))
+        print('Erro geral', str(e))
         sys.exit()
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+## importar a biblioteca para usar socket e biblioteca select para administrar varias conexões
 import select
 import socket
 
@@ -5,64 +6,72 @@ HEARDER_SIZE = 10
 IP = "127.0.0.1"
 PORT = 4200
 
+##Criação dos sockets 
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 socket_server.bind((IP, PORT))
 socket_server.listen()
 
+## Uma lista de clientes em forma de socket, atribuido ao server pois quando os clientes se conectarem estarao ali
 socket_list = [socket_server]
 
 clients = {}
 
 
+##Funcao basica para receber mensagem
 def receive_message(client_socket):
     try:
         message_header = client_socket.recv(HEARDER_SIZE)
         if not len(message_header):
-            return False
+            return False  ##Fecha a conexão se não receber nada 
 
         message_len = int(message_header.decode("utf-8").strip())
         return {"header": message_header, "data": client_socket.recv(message_len)}
     except:
-        return False
+        return False  ##Fecha a conexão
 
 
 while True:
     read_sockets, _, exception_sockets = select.select(
-        socket_list, [], socket_list)
+        socket_list, [], socket_list) ## De onde conseguimos os dados
 
+    ##Cliente conectou e vamos gerenciar essa conexao
     for notified_socket in read_sockets:
         if notified_socket == socket_server:
             client_socket, client_address = socket_server.accept()
 
-            user = receive_message(client_socket)
-            if user is False:
+            user = receive_message(client_socket) ##Recebe da funcao anterior basica que criamos
+            if user is False: ##Cliente desconectou
                 continue
 
-            socket_list.append(client_socket)
-            clients[client_socket] = user
+            socket_list.append(client_socket) ##adiocina na lista o cliente
+            clients[client_socket] = user #adiciona as infomacoes do cliente na lista de cliente
 
+            ##Mostra na tela que aceitou a conexão de um novo cliente
             print(
-                f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+                f"Nova conexão de: {client_address[0]}:{client_address[1]} usuario:{user['data'].decode('utf-8')}")
         else:
             message = receive_message(notified_socket)
 
+            ##Caso de algum erro prepara uma mensagem de erro e fala que conexão finalizada e tira o cliente da lista
             if message is False:
                 print(
-                    f"closed connection from {clients[notified_socket]['data'].decode('utef-8')}")
-                socket_list.remove(notified_socket)
+                    f"Conexao fechada de: {clients[notified_socket]['data'].decode('utef-8')}")
+                socket_list.remove(notified_socket) ##retira o cliente da lista
                 del clients[notified_socket]
                 continue
 
-            user = clients[notified_socket]
+            user = clients[notified_socket] 
+            ##Mostra na tela de quem recebeu a mensagem
             print(
-                f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+                f"Nova mensagem de: {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
 
             for client_socket in clients:
                 if client_socket != notified_socket:
                     client_socket.send(
-                        user['header'] + user['data'] + message['header'] + message['data'])
+                        user['header'] + user['data'] + message['header'] + message['data']) ##manda os dados do cliente e sua mensagem separadas
+            ##Remove da lista o cliente
     for notified_socket in exception_sockets:
-        socket_list.remove(notified_socket)
+        socket_list.remove(notified_socket) 
         del clients[notified_socket]
